7ad540e6c10d5bade629c3f8aeeb40055467f870
worms19 <anselmidamien@gmail.com>
2020-01-23 15:21:35 +0100

    Finish styling header and body

---
+++ src/components/App/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.scss';
+import NumberDisplay from "../NumberDisplay";
+
+const App :React.FC = () => {
+    return (
+        <div className="App">
+            <div className="Header">
+                <NumberDisplay
+                    value={0}
+                />
+
+                <div className="Face"><span role="img" aria-label="face"> 😂</span></div>
+                <NumberDisplay
+                    value={23}
+                />
+
+            </div>
+            <div className="Body">Body</div>
+        </div>
+    )
+};
+
+export default App;
