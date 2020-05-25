ec7053086282b5ae66955730f1f1b76124f5cae6
worms19 <anselmidamien@gmail.com>
2020-05-26 01:22:20 +0200

    Finished styling the body and the cells

---
+++ src/utils/index.tsx
@@ -0,0 +1,19 @@
+import {MAX_ROWS} from "../../constants";
+import {Cell, CellState, CellValue} from "../../types";
+
+export const generateCells = (): Cell[][] => {
+  const cells: Cell[][] = [];
+
+  for (let row=0; row < MAX_ROWS; row++){
+    cells.push([]);
+    for (let col=0; col < MAX_ROWS; col++){
+      cells[row].push({
+        value: CellValue.none,
+        state: CellState.open
+      });
+    }
+  }
+
+  return cells;
+};
+
